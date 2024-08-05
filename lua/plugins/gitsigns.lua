df4d6b599fd7125b1c80aad595c9b7b6a9698558
gabs712 <gabs712.g@gmail.com>
2024-08-05 17:20:06 -0300

    Add gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,5 +2,8 @@ return {
   'lewis6991/gitsigns.nvim',
   config = function()
     require('gitsigns').setup()
+
+    vim.keymap.set('n', '<leader>gp', '<cmd>Gitsigns preview_hunk<CR>')
+    vim.keymap.set('n', '<leader>gt', '<cmd>Gitsigns toggle_current_line_blame<CR>')
   end
 }
