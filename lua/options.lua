939d969195763d7931719de3d3f37e0526ba2174
gabs712 <gabs712.g@gmail.com>
2024-08-04 21:39:06 -0300

    Add options and tokyonight

---
+++ lua/options.lua
@@ -0,0 +1,47 @@
+-- Must be set before plugins are loaded
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+-- Follow identation on wrap
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Open space to line symbols
+vim.opt.signcolumn = 'yes'
+
+-- Decrease custom mapped sequence wait time -- Default is 1000
+vim.opt.timeoutlen = 1000
+
+-- Change position of new splits
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Show empty chars
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Visual feedback when using :s
+vim.opt.inccommand = 'split'
+
+-- Highlight selected line
+vim.opt.cursorline = true
+
+-- How many lines keep around cursor when moving vertically
+vim.opt.scrolloff = 15
+
+-- Convert tabs to spaces
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+
+-- Use status bar globally
+vim.opt.laststatus = 3
+
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,42 +1,42 @@
 return {
-	{
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-	"nvim-lua/plenary.nvim",
-	"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-	"MunifTanjim/nui.nvim",
-},
-config = function()
-	vim.cmd('Neotree reveal show left')
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      vim.cmd('Neotree reveal show left')
 
-	require('neo-tree').setup({
-		close_if_last_window = true,
-	})
+      require('neo-tree').setup({
+        close_if_last_window = true,
+      })
 
-	vim.keymap.set('n', '<leader>E', '<cmd>Neotree toggle reveal show left<CR>')
-	vim.keymap.set('n', '<leader>e', function()
-		-- If a tree is being foucused, go to previous window
-		if vim.bo.filetype:match('^neo%-tree') then
-			vim.cmd('wincmd p')
-			return
-		end
+      vim.keymap.set('n', '<leader>E', '<cmd>Neotree toggle reveal show left<CR>')
+      vim.keymap.set('n', '<leader>e', function()
+        -- If a tree is being foucused, go to previous window
+        if vim.bo.filetype:match('^neo%-tree') then
+          vim.cmd('wincmd p')
+          return
+        end
 
-		-- If there is a tree, focus it
-		for _, win in ipairs(vim.api.nvim_list_wins()) do
-			local buf = vim.api.nvim_win_get_buf(win)
-			local bufname = vim.api.nvim_buf_get_name(buf)
-			local filetype = vim.api.nvim_buf_get_option(buf, 'filetype')
+        -- If there is a tree, focus it
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          local bufname = vim.api.nvim_buf_get_name(buf)
+          local filetype = vim.api.nvim_buf_get_option(buf, 'filetype')
 
-			if filetype == 'neo-tree' then
-				vim.api.nvim_set_current_win(win)
-				return
-			end
-		end	
+          if filetype == 'neo-tree' then
+            vim.api.nvim_set_current_win(win)
+            return
+          end
+        end
 
-		-- If there is not a tree, create a new one
-		vim.cmd('Neotree reveal focus left')
-	end)
-end
-	}
+        -- If there is not a tree, create a new one
+        vim.cmd('Neotree reveal focus left')
+      end)
+    end
+  }
 }
