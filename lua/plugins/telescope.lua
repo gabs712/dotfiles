aef65a5a6832564c9116e910b8c99d0c1144cf37
gabs712 <gabs712.g@gmail.com>
2024-08-06 01:08:45 -0300

    Add keymap to find keys

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
+
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files)
     vim.keymap.set('n', '<leader>fa', builtin.live_grep)
@@ -17,6 +18,7 @@ return {
         prompt_title = 'Neovim Files'
       }
     end)
+    vim.keymap.set('n', '<leader>fk', builtin.keymaps)
   end
 }
 
