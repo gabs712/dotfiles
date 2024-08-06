9449dbd17b4b07f64d53a5ea1a29ef4e1bbfecab
gabs712 <gabs712.g@gmail.com>
2024-08-05 21:00:58 -0300

    Change git signs in order to mantain consistency across colorschemes

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,7 +1,15 @@
 return {
   'lewis6991/gitsigns.nvim',
   config = function()
-    require('gitsigns').setup()
+    require('gitsigns').setup({
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      }
+    })
 
     vim.keymap.set('n', '<leader>gp', '<cmd>Gitsigns preview_hunk<CR>')
     vim.keymap.set('n', '<leader>gt', '<cmd>Gitsigns toggle_current_line_blame<CR>')
