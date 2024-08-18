0e1563ab09342b0430e540719536e210f7c2e71f
gabs <gabs712.g@gmail.com>
2024-08-18 01:52:18 -0300

    Customize lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,19 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('lualine').setup()
+    require('lualine').setup({
+      options = {
+        component_separators = {left = '', right = ''},
+        section_separators = {left = '', right = ''},
+      },
+      sections = {
+        lualine_a = {},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        lualine_x = {'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {},
+      },
+    })
   end
 }
