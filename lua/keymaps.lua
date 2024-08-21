14918ff100d8a0ab52f17801d68700bce70060e4
gabs <gabs712.g@gmail.com>
2024-08-21 02:55:03 -0300

    Handle string whitespaces with around

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,11 +6,19 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Window bottom' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Window top' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Window right' })
 
-vim.keymap.set('n', '<Space>', '<Nop>', { desc = 'Disable default space' })
+vim.keymap.set('n', '<Space>', '<Nop>')
 
--- window resize
+-- Window resize
 vim.keymap.set("n", "<C-Up>", '<cmd>vertical resize +5<cr>', { desc = 'Window vertical increase' })
 vim.keymap.set("n", "<C-Down>", '<cmd>vertical resize -5<cr>', { desc = 'Window vertical decrease' })
 vim.keymap.set("n", "<C-Right>", '<cmd>horizontal resize +2<cr>', { desc = 'Window horizontal increase' })
 vim.keymap.set("n", "<C-Left>", '<cmd>horizontal resize -2<cr>', { desc = 'Window horizontal decrease' })
+
+-- With strings, when using "a" on operator-pending or visual mode, not include extra whitespace
+vim.keymap.set('o', "a'", "2i'")
+vim.keymap.set('o', 'a"', "2i\"")
+vim.keymap.set('o', 'a`', "2i`")
+vim.keymap.set('x', "a'", "2i'")
+vim.keymap.set('x', 'a"', "2i\"")
+vim.keymap.set('x', 'a`', "2i`")
 
