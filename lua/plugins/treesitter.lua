55f17c98dfa16217996595abb4d00ade614834cc
gabs <gabs712.g@gmail.com>
2024-08-22 02:01:23 -0300

    Add textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,26 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = 'nvim-treesitter/nvim-treesitter-textobjects',
   build = ":TSUpdate",
   config = function () 
     require("nvim-treesitter.configs").setup({
       ensure_installed = { 'lua', 'javascript', 'html', 'css', 'typescript' },
       auto_install = true,
-      sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
+
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true, -- Jump to next ocurrence if not inside any
+
+          keymaps = {
+            ["if"] = { query = "@function.inner", desc = "Select inside function" },
+            ["af"] = { query = "@function.outer", desc = "Select around function" },
+            ["ic"] = { query = "@class.inner", desc = "Select inside class" },
+          },
+        }
+      }
     })
   end
 }
