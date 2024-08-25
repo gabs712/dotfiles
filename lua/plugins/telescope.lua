3a4eb877133cc77a79ddaef1518b8eb4b5041a25
gabs <gabs712.g@gmail.com>
2024-08-25 20:51:20 -0300

    Close telescope when pressing esc

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,8 +2,19 @@ return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-
     local builtin = require('telescope.builtin')
+    local actions = require("telescope.actions")
+
+    require("telescope").setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<esc>"] = actions.close
+            },
+        },
+      }
+    })
+
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'Find files' })
     vim.keymap.set('n', '<C-f>', builtin.find_files, {desc = 'Find files' })
     vim.keymap.set('n', '<leader>fa', builtin.live_grep, {desc = 'Find all terms' })
@@ -13,13 +24,13 @@ return {
     vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {desc = 'Find recent' })
     vim.keymap.set('n', '<leader>fc', builtin.colorscheme, {desc = 'Find colorschemes' })
     vim.keymap.set('n', '<C-/>', builtin.current_buffer_fuzzy_find, {desc = 'Search in buffer' })
+    vim.keymap.set('n', '<leader>fk', builtin.keymaps, {desc = 'Find keymaps' })
     vim.keymap.set('n', '<leader>fn', function()
       builtin.find_files {
         cwd = vim.fn.stdpath 'config',
         prompt_title = 'Neovim Files'
       }
     end, { desc = 'Find neovim config' })
-    vim.keymap.set('n', '<leader>fk', builtin.keymaps, {desc = 'Find keymaps' })
   end
 }
 
