10f3695405298b0aa0f4d67e5b50bb42ad664a00
gabs <gabs712.g@gmail.com>
2024-08-28 19:24:56 -0300

    Clean up colorschemes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -2,15 +2,11 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    priority = 900,
   },
   {
     "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 900,
   },
   {
     "Mofiqul/dracula.nvim",
-    priority = 900,
   },
 }
