c47282a6229d0c1ca8a613bfe333ed2d43dea0b7
gabs <gabs712.g@gmail.com>
2024-08-29 19:22:00 -0300

    Disable automatic new line comments only to 'o'

--- lua/options.lua
+++ lua/options.lua
@@ -64,7 +64,7 @@ vim.opt.showmode = false
 -- Disable comment insertion when pressing 'o' or '<CR>' next to it
 vim.api.nvim_create_autocmd('BufEnter', {
   callback = function()
-    vim.opt.formatoptions:remove({ 'c', 'r', 'o' })
+    vim.opt.formatoptions:remove({'o'})
   end,
 })
 
