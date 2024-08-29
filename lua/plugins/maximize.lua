690b51432d9620d51e4bfa4f9b9f316d25f2457a
gabs <gabs712.g@gmail.com>
2024-08-29 19:00:20 -0300

    Add maxmize to it's own module

---
+++ lua/plugins/maximize.lua
@@ -0,0 +1,10 @@
+-- Maximize Icon added to lualine.lua
+return {
+  'declancm/maximize.nvim',
+  config = function()
+    require('maximize').setup()
+
+    vim.keymap.set({'n', 'x'}, '<C-w>m', require('maximize').toggle, {desc = 'Toggle window maximize'})
+  end
+}
+
--- lua/plugins/tweaks.lua
+++ lua/plugins/tweaks.lua
@@ -23,13 +23,5 @@ return {
         }
       })
     end
-  },
-  {
-    'declancm/maximize.nvim',
-    config = function()
-      require('maximize').setup()
-
-      vim.keymap.set({'n', 'x'}, '<C-w>m', require('maximize').toggle, {desc = 'Toggle window maximize'})
-    end
-  },
+  }
 }
