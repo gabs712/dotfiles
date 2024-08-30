f7fe4d9a96172501f15a82c81dce6369e945adf6
gabs <gabs712.g@gmail.com>
2024-08-30 19:34:36 -0300

    Improve lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -10,26 +10,30 @@ return {
     require('lualine').setup({
       options = {
         theme = 'auto',
+        disabled_filetypes = {
+          statusline = {}, -- Only ignores the ft for statusline.
+          winbar = {}, -- Only ignores the ft for winbar.
+          -- Ignores for both \/
+        },
 
         -- Some icons:             
         section_separators = {left = '', right = ''},
-        component_separators = {left = '', right = ''},
+        component_separators = {left = '', right = ''},
       },
       sections = {
         lualine_a = {},
         lualine_b = {
-          'branch', 
-          {
-            'diff',
-            symbols = {
-              added = '+',
-              modified = '~',
-              removed = '-'
-            },
-          },
-          'diagnostics'
+          'branch',
         },
         lualine_c = {
+          {
+            'filetype',
+            padding = {left = 1, right = 0},
+            icon_only = true,
+            icon = {
+              align = 'left'
+            }
+          },
           {
             'filename',
             path = 4, -- Parent directory and file name
@@ -37,23 +41,46 @@ return {
               -- Icons:  
               modified = '',
               readonly = '',
-              unnamed = '-',
+              unnamed = 'Unnamed',
               newfile = '',
             }
           },
         },
         lualine_x = {
-          'searchcount',
-          maximize_icon,
           {
-            'filetype',
-            icon_only = false,
-            icon = {
-              align = 'left'
-            }
+            'searchcount',
+            padding = {left = 1, right = 3}
+          },
+          'diagnostics',
+          {
+            'diff',
+            padding = {left = 1, right = 1},
+            symbols = {
+              added = ' ',
+              modified = ' ',
+              removed = ' '
+            },
+            -- Where to get git info from
+            source = function()
+              -- Using gitsigns, it updates in real time, with no need to save
+              local gitsigns = vim.b.gitsigns_status_dict
+              if gitsigns then
+                return {
+                  added = gitsigns.added,
+                  modified = gitsigns.changed,
+                  removed = gitsigns.removed,
+                }
+              end
+            end,
           },
         },
-        lualine_y = {'progress'},
+        lualine_y = {
+          {
+            padding = {left = 1, right = 0},
+            maximize_icon,
+          },
+          'progress',
+        },
         lualine_z = {},
       },
     })
