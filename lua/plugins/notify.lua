9337acf3b3222353c11625aae909485cb1f00404
gabs <gabs712.g@gmail.com>
2024-09-01 23:51:38 -0300

    Tweak notify

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,16 +2,16 @@ return {
   "rcarriga/nvim-notify",
   config = {
     require('notify').setup({
-      timeout = 1500,
+      timeout = 3000,
       stages = 'static', -- static, fade, fade_in_slide_out, slide
       render = 'minimal', -- minimal, wrapped-compact, compact, simple, default
-      minimum_width = 43,
+      minimum_width = 0, -- 43
 
       max_height = function()
-        return math.floor(vim.o.lines * 0.40)
+        return math.floor(vim.o.lines * 0.75)
       end,
       max_width = function()
-        return math.floor(vim.o.columns * 0.40)
+        return math.floor(vim.o.columns * 0.75)
       end,
     })
   }
