804f245f6bd24da2c3e921c4c26827f3d905cdef
gabs <gabs712.g@gmail.com>
2024-09-01 23:33:25 -0300

    Add dependency guidance comments

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 -- Config initially developed on NVIM v0.11.0-dev
+
+-- Run "sudo apt install make gcc ripgrep unzip git xclip"
 -- Run "xset r rate 560 25" to decrease cursor movement delay
 
 require('options')
