177f356c46a9c5441478f362856b20b330c2f122
gabs <gabs712.g@gmail.com>
2024-09-14 12:01:19 -0300

    Wrap lsp keymaps on lspAttach

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -35,12 +35,16 @@ return {
       end,
     })
 
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {desc = 'Go to definition'})
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {desc = 'Manual about symbol under cursor'})
-    vim.keymap.set('n', '<leader>p', vim.lsp.buf.format, {desc = 'Format (make pretty)'})
-    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, {desc = 'Rename symbol across project'})
-    vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, {desc = 'Code actions'})
+    -- ev.buf ensures that lsp keymaps are only settled when a language server is avaliable
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(ev)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = ev.buf, desc = 'Go to definition' })
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = ev.buf, desc = 'Manual about symbol under cursor' })
+        vim.keymap.set('n', '<leader>p', vim.lsp.buf.format, { buffer = ev.buf, desc = 'Format (make pretty)' })
+        vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { buffer = ev.buf, desc = 'Rename symbol across project' })
+        vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, { buffer = ev.buf, desc = 'Code actions' })
+      end
+    })
   end
 }
 
--- kickstart linha 590
