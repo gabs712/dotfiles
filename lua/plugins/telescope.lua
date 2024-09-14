ad58b0efdc8be95ef50748ca90d9c64977a1fae6
gabs <gabs712.g@gmail.com>
2024-09-14 10:08:33 -0300

    Use dressing instead of telescope ui-select

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,6 @@ return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-telescope/telescope-ui-select.nvim'
   },
   config = function()
     local builtin = require('telescope.builtin')
@@ -16,14 +15,7 @@ return {
           },
         },
       },
-      extensions = {
-        ['ui-select'] = {
-          require("telescope.themes").get_dropdown() -- Change prompt style
-        }
-      }
     })
-
-    require("telescope").load_extension("ui-select")
 
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'Find files' })
     vim.keymap.set({'n', 'x'}, '<C-f>', builtin.find_files, {desc = 'Find files' })
