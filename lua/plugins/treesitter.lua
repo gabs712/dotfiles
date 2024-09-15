4c3046c54972d2c82758562ce50367abc01984ed
gabs <gabs712.g@gmail.com>
2024-09-14 23:33:34 -0300

    Add vimdoc to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   build = ":TSUpdate",
   config = function () 
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { 'lua', 'javascript', 'html', 'css', 'typescript' },
+      ensure_installed = { 'lua', 'javascript', 'html', 'css', 'typescript', 'vimdoc' },
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
