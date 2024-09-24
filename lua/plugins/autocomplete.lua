5ed842b820b4f0e0743de722352706b84e55dce9
gabs <gabs712.g@gmail.com>
2024-09-23 21:55:16 -0300

    Remove recently used from html/css

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -185,7 +185,7 @@ return {
           cmp.config.compare.exact,
           cmp.config.compare.scopes,
           cmp.config.compare.score,
-          cmp.config.compare.recently_used,
+          -- cmp.config.compare.recently_used,
           cmp.config.compare.locality,
           cmp.config.compare.kind,
           cmp.config.compare.sort_text,
