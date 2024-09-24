3faf6db889fba7a6af8871a5c8fb92bdc349a731
gabs <gabs712.g@gmail.com>
2024-09-23 21:57:15 -0300

    Add buffer as source

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -33,6 +33,7 @@ return {
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
+        { name = 'buffer' },
         { name = 'path' },
       }),
       formatting = {
