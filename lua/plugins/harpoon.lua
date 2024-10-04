7d846c464ba2f9d5c09e73bcdee5d6f6f9e0eea7
gabs <gabs712.g@gmail.com>
2024-10-04 02:51:36 -0300

    Remove telescope integration from harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,7 +1,7 @@
 return {
   'ThePrimeagen/harpoon',
   branch = 'harpoon2',
-  dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
+  dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local harpoon = require('harpoon')
     harpoon:setup({
@@ -30,27 +30,5 @@ return {
     vim.keymap.set({ 'n', 'x' }, '<C-h>', function()
       harpoon:list():select(4)
     end, { desc = 'Go to harpoon mark 4' })
-
-    -- Telescope configuration
-    vim.keymap.set('n', '<leader>fm', function()
-      local conf = require('telescope.config').values
-      local harpoon_files = harpoon:list()
-      local file_paths = {}
-
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-      end
-
-      require('telescope.pickers')
-        .new({}, {
-          prompt_title = 'Harpoon Marks',
-          finder = require('telescope.finders').new_table({
-            results = file_paths,
-          }),
-          previewer = conf.file_previewer({}),
-          sorter = conf.generic_sorter({}),
-        })
-        :find()
-    end, { desc = 'Find harpoon marks' })
   end,
 }
