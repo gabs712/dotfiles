457b651ab8102f766993c5910a2b19d00d1886c6
gabs <gabs712.g@gmail.com>
2024-10-06 18:27:15 -0300

    Improve on gitsigns keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,19 +1,26 @@
 return {
   'lewis6991/gitsigns.nvim',
   config = function()
-    require('gitsigns').setup()
+    local gitsigns = require('gitsigns')
+    gitsigns.setup()
 
-    vim.keymap.set('n', '<leader>h', '<cmd>Gitsigns preview_hunk<CR>', { desc = 'Git preview hunk' })
-    vim.keymap.set('n', '<leader>H', '<cmd>Gitsigns diffthis<CR>', { desc = 'Git diff' })
+    vim.keymap.set('n', '<leader>h', gitsigns.preview_hunk, { desc = 'Git preview hunk' })
+    vim.keymap.set('n', '<leader>H', function()
+      gitsigns.diffthis(nil, { vertical = true, split = 'rightbelow' })
+    end, { desc = 'Git hunk diff' })
 
-    vim.keymap.set('n', '<leader>b', '<cmd>Gitsigns toggle_current_line_blame<CR>', { desc = 'Git toggle blame' })
-    vim.keymap.set('n', '<leader>B', '<cmd>Gitsigns blame<CR>', { desc = 'Git blame' })
+    vim.keymap.set('n', '<leader>b', gitsigns.blame_line, { desc = 'Git preview blame' })
+    vim.keymap.set('n', '<leader>B', gitsigns.blame, { desc = 'Git blame' })
 
-    vim.keymap.set('n', ']h', '<cmd>Gitsigns next_hunk<CR>', { desc = 'Git next hunk' })
-    vim.keymap.set('n', '[h', '<cmd>Gitsigns prev_hunk<CR>', { desc = 'Git previous hunk' })
+    vim.keymap.set('n', ']h', function()
+      gitsigns.nav_hunk('next')
+    end, { desc = 'Git next hunk' })
+    vim.keymap.set('n', '[h', function()
+      gitsigns.nav_hunk('prev')
+    end, { desc = 'Git previous hunk' })
 
-    vim.keymap.set('o', 'ah', '<cmd>Gitsigns select_hunk<CR>', { desc = 'Select hunk' })
-    vim.keymap.set('o', 'ih', '<cmd>Gitsigns select_hunk<CR>', { desc = 'Select hunk' })
+    vim.keymap.set('o', 'ah', gitsigns.select_hunk, { desc = 'Select hunk' })
+    vim.keymap.set('o', 'ih', gitsigns.select_hunk, { desc = 'Select hunk' })
 
     -- Uses ':' in visual mode due to a bug (Only lines after cursor were being selected)
     vim.keymap.set('x', 'ah', ':Gitsigns select_hunk<CR>', { desc = 'Select hunk' })
