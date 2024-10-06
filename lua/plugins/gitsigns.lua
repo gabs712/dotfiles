fce710255cf153ca1dee0604b1c632367a1b505b
gabs <gabs712.g@gmail.com>
2024-10-05 22:09:58 -0300

    Enhance gitsigns keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,7 +3,20 @@ return {
   config = function()
     require('gitsigns').setup()
 
-    vim.keymap.set('n', '<leader>gp', '<cmd>Gitsigns preview_hunk<CR>', { desc = 'Git preview' })
-    vim.keymap.set('n', '<leader>gt', '<cmd>Gitsigns toggle_current_line_blame<CR>', { desc = 'Git toggle blame' })
+    vim.keymap.set('n', '<leader>h', '<cmd>Gitsigns preview_hunk<CR>', { desc = 'Git preview hunk' })
+    vim.keymap.set('n', '<leader>H', '<cmd>Gitsigns diffthis<CR>', { desc = 'Git diff' })
+
+    vim.keymap.set('n', '<leader>b', '<cmd>Gitsigns toggle_current_line_blame<CR>', { desc = 'Git toggle blame' })
+    vim.keymap.set('n', '<leader>B', '<cmd>Gitsigns blame<CR>', { desc = 'Git blame' })
+
+    vim.keymap.set('n', ']h', '<cmd>Gitsigns next_hunk<CR>', { desc = 'Git next hunk' })
+    vim.keymap.set('n', '[h', '<cmd>Gitsigns prev_hunk<CR>', { desc = 'Git previous hunk' })
+
+    vim.keymap.set('o', 'ah', '<cmd>Gitsigns select_hunk<CR>', { desc = 'Select hunk' })
+    vim.keymap.set('o', 'ih', '<cmd>Gitsigns select_hunk<CR>', { desc = 'Select hunk' })
+
+    -- Uses ':' in visual mode due to a bug (Only lines after cursor were being selected)
+    vim.keymap.set('x', 'ah', ':Gitsigns select_hunk<CR>', { desc = 'Select hunk' })
+    vim.keymap.set('x', 'ih', ':Gitsigns select_hunk<CR>', { desc = 'Select hunk' })
   end,
 }
