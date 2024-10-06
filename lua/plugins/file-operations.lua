8fac7a98a34054e3f093f586a777aa0ebb627685
gabs <gabs712.g@gmail.com>
2024-10-06 02:39:07 -0300

    Add nvim-file-operations

---
+++ lua/plugins/file-operations.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    -- Updates imports when renaming files with supported plugins
+    'antosha417/nvim-lsp-file-operations',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-neo-tree/neo-tree.nvim',
+    },
+    config = function()
+      require('lsp-file-operations').setup()
+    end,
+  },
+}
