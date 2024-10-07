39bd4e569922e229144ebf252608e76538cf3486
gabs <gabs712.g@gmail.com>
2024-10-07 04:27:10 -0300

    Decrease max width for notify

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -11,7 +11,7 @@ return {
         return math.floor(vim.o.lines * 0.75)
       end,
       max_width = function()
-        return math.floor(vim.o.columns * 0.75)
+        return math.floor(vim.o.columns * 0.40)
       end,
     })
   end,
