aedad528230e14aa2501eeec7da6b4e59e54ddf5
gabs <gabs712.g@gmail.com>
2024-10-07 23:33:55 -0300

    Update deprecated config for ts-autotag

--- lua/plugins/autotag.lua
+++ lua/plugins/autotag.lua
@@ -2,7 +2,9 @@ return {
   'windwp/nvim-ts-autotag',
   config = function()
     require('nvim-ts-autotag').setup({
-      enable_close_on_slash = true,
+      opts = {
+        enable_close_on_slash = true,
+      },
     })
   end,
 }
