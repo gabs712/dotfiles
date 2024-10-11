80002ba7188e79e4707426c69b7b183641e931ce
gabs <gabs712.g@gmail.com>
2024-10-11 16:02:30 -0300

    Add todo-comments

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,20 @@
+return {
+  'folke/todo-comments.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+  },
+  config = function()
+    local todo_comments = require('todo-comments')
+
+    todo_comments.setup({
+      signs = false,
+      highlight = {
+        -- keyword = '',
+        -- after = '',
+      },
+    })
+
+    vim.keymap.set('n', '<leader>ft', '<cmd>TodoTelescope<CR>', { desc = 'Find todos' })
+  end,
+}
