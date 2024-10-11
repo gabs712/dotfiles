3f3db5a64dc08950cd7e6f4f2fce5335ffa68ac7
gabs <gabs712.g@gmail.com>
2024-10-11 12:26:26 -0300

    Add telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,9 +19,16 @@ return {
       },
     })
 
+    vim.keymap.set('n', '<leader>ff', function()
+      builtin.find_files({ hidden = true, prompt_title = 'Find Files (hidden)' })
+    end, { desc = 'Find files including hidden' })
+
+    vim.keymap.set('n', '<leader>fa', function()
+      builtin.find_files({ hidden = true, no_ignore = true, prompt_title = 'Find All Files' })
+    end, { desc = 'Find all files' })
+
     vim.keymap.set({ 'n', 'x' }, '<C-f>', builtin.find_files, { desc = 'Find files' })
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
-    vim.keymap.set('n', '<leader>fa', builtin.live_grep, { desc = 'Find all strings (grep)' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Find all strings (grep)' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Find help' })
     vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = 'Find old files' })
     vim.keymap.set('n', '<leader>fc', builtin.colorscheme, { desc = 'Find colorschemes' })
@@ -39,6 +46,6 @@ return {
     vim.keymap.set('n', '<leader>m', builtin.marks, { desc = 'Find marks' })
 
     vim.keymap.set('n', 'gd', builtin.lsp_definitions, { desc = 'Go to definition' })
-    vim.keymap.set('n', 'gr', builtin.lsp_references, { desc = 'Find references' })
+    vim.keymap.set('n', 'gr', builtin.lsp_references, { desc = 'Go to references' })
   end,
 }
