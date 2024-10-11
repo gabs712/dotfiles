4cd8bf63213161b37699eefe8480aeb9a025d41e
gabs <gabs712.g@gmail.com>
2024-10-10 23:50:38 -0300

    Tweak telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,22 +19,22 @@ return {
       },
     })
 
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
     vim.keymap.set({ 'n', 'x' }, '<C-f>', builtin.find_files, { desc = 'Find files' })
-    vim.keymap.set('n', '<leader>fa', builtin.live_grep, { desc = 'Find all terms' })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+    vim.keymap.set('n', '<leader>fa', builtin.live_grep, { desc = 'Find all strings (grep)' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Find help' })
-    vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Find git files' })
     vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = 'Find old files' })
     vim.keymap.set('n', '<leader>fc', builtin.colorscheme, { desc = 'Find colorschemes' })
-    vim.keymap.set('n', '<leader>f/', builtin.current_buffer_fuzzy_find, { desc = 'Search in buffer' })
     vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Find keymaps' })
+    vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = 'Find symbols' })
     vim.keymap.set('n', '<leader>fn', function()
       builtin.find_files({
         cwd = vim.fn.stdpath('config'),
         prompt_title = 'Neovim Files',
       })
     end, { desc = 'Find neovim config' })
+
+    vim.keymap.set('n', '<C-/>', builtin.current_buffer_fuzzy_find, { desc = 'Search in buffer' })
     vim.keymap.set('n', "<leader>'", builtin.registers, { desc = 'Find registers' })
     vim.keymap.set('n', '<leader>m', builtin.marks, { desc = 'Find marks' })
 
