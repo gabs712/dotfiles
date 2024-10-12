cc508a8c88f8d5b70c7fbd9d7eb61a0a8a201ac9
gabs <gabs712.g@gmail.com>
2024-10-12 20:24:09 -0300

    Fix smart j/k on visual modes

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -37,9 +37,10 @@ vim.keymap.set({ 'n', 'o', 'x' }, '_', '^')
 vim.keymap.set({ 'n', 'o', 'x' }, 'g_', 'g^')
 
 -- Move through wrap and use normal j/k when providing a counter
--- TODO: Not working on normal mode
-vim.keymap.set({ 'n', 'x' }, "v:count == 0 ? 'gj' : 'j'", 'gj')
-vim.keymap.set({ 'n', 'x' }, "v:count == 0 ? 'gk' : 'k'", 'gk')
+vim.cmd("nnoremap <expr> k v:count == 0 ? 'gk' : 'k'")
+vim.cmd("nnoremap <expr> j v:count == 0 ? 'gj' : 'j'")
+vim.cmd("xnoremap <expr> k v:count == 0 ? 'gk' : 'k'")
+vim.cmd("xnoremap <expr> j v:count == 0 ? 'gj' : 'j'")
 
 -- Add relative jumps to jump list
 vim.cmd("nnoremap <expr> j v:count ? (v:count > 0 ? \"m'\" . v:count : '') . 'j' : 'gj'")
