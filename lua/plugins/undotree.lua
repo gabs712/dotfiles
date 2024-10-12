3490d904ab01aa5b0bcbee346e6e74c23171b871
gabs <gabs712.g@gmail.com>
2024-10-12 10:29:33 -0300

    Change undo tree plugin

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,29 +1,6 @@
 return {
-  'jiaoshijie/undotree',
-  dependencies = 'nvim-lua/plenary.nvim',
+  'mbbill/undotree',
   config = function()
-    local undotree = require('undotree')
-
-    undotree.setup({
-      position = 'right',
-      keymaps = {
-        ['j'] = 'move_next',
-        ['k'] = 'move_prev',
-        ['gj'] = 'move2parent',
-        ['<Esc>'] = 'move2parent',
-        ['u'] = 'move_change_next',
-        ['<C-r>'] = 'move_change_prev',
-        ['<C-n>'] = 'move_change_next',
-        ['<C-p>'] = 'move_change_prev',
-        ['J'] = 'move_change_next',
-        ['K'] = 'move_change_prev',
-        ['<cr>'] = 'action_enter',
-        ['<C-j>'] = 'action_enter',
-        ['p'] = 'enter_diffbuf',
-        ['q'] = 'quit',
-      },
-    })
-
-    vim.keymap.set({ 'n', 'x' }, '<leader>u', undotree.toggle)
+    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Undotree' })
   end,
 }
