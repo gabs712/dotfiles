9e8eaacdf597975da8e9e75f1d5cf9186e98c899
gabs <gabs712.g@gmail.com>
2024-10-13 00:03:55 -0300

    Enhance undotree config

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,7 +1,22 @@
--- TODO: Enhance config
 return {
   'mbbill/undotree',
   config = function()
+    vim.g.undotree_WindowLayout = 3
+    vim.g.undotree_SplitWidth = 44
+    vim.g.undotree_DiffAutoOpen = 0 -- 0, 1
+    vim.g.undotree_SetFocusWhenToggle = 0
+    vim.g.undotree_ShortIndicators = 0 -- s, h, m, etc
+    vim.g.undotree_HighlightChangedText = 0
+    vim.g.undotree_HelpLine = 1
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'undotree',
+      callback = function()
+        vim.keymap.set('n', '<C-->', '<plug>UndotreePreviousState', { buffer = vim.api.nvim_get_current_buf() })
+        vim.keymap.set('n', '<C-=>', '<plug>UndotreeNextState', { buffer = vim.api.nvim_get_current_buf() })
+      end,
+    })
+
     vim.keymap.set({ 'n', 'x' }, '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Undotree' })
   end,
 }
