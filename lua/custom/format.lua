e3f49ce020c591b350c10f4fd215c22a23963037
gabs <gabs712.g@gmail.com>
2024-10-14 22:58:13 -0300

    Clean up null ls config

---
+++ lua/custom/format.lua
@@ -0,0 +1,39 @@
+-- Used by null-ls
+local M = {}
+
+-- Formats only using null_ls sources
+local null_format = function()
+  vim.lsp.buf.format({
+    filter = function(client)
+      return client.name == 'null-ls'
+    end,
+  })
+end
+
+M.format = function()
+  -- Don't format if there isn't any lsp clients attached
+  local buffer_clients = vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() })
+  if #buffer_clients == 0 then
+    return
+  end
+
+  null_format()
+end
+
+-- Format and sort with tailwind
+M.sort_format = function()
+  local buffer_clients = vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() })
+  if #buffer_clients == 0 then
+    return
+  end
+
+  null_format()
+  for _, client in ipairs(buffer_clients) do
+    if client.name == 'tailwindcss' then
+      vim.cmd('TailwindSort')
+      return
+    end
+  end
+end
+
+return M
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -20,42 +20,7 @@ return {
       },
     })
 
-    -- Formats only using null_ls sources
-    local null_format = function()
-      vim.lsp.buf.format({
-        filter = function(client)
-          return client.name == 'null-ls'
-        end,
-      })
-    end
-
-    local format = function()
-      -- Don't format if there isn't any lsp clients attached
-      local buffer_clients = vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() })
-      if #buffer_clients == 0 then
-        return
-      end
-
-      null_format()
-    end
-
-    -- Format and sort with tailwind
-    local format_and_sort = function()
-      local buffer_clients = vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() })
-      if #buffer_clients == 0 then
-        return
-      end
-
-      null_format()
-      for _, client in ipairs(buffer_clients) do
-        if client.name == 'tailwindcss' then
-          vim.cmd('TailwindSort')
-          return
-        end
-      end
-    end
-
-    vim.api.nvim_create_autocmd('BufWritePre', { callback = format })
-    vim.keymap.set({ 'n', 'x' }, '<leader>p', format_and_sort, { desc = 'Format (make pretty)' })
+    vim.api.nvim_create_autocmd('BufWritePre', { callback = require('custom.format').format })
+    vim.keymap.set({ 'n', 'x' }, '<leader>p', require('custom.format').sort_format, { desc = 'Format (make pretty)' })
   end,
 }
