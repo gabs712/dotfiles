55b95b3d4972d62f615e8febec97d93a4761d900
gabs <gabs712.g@gmail.com>
2024-10-14 21:15:04 -0300

    Add outline

---
+++ lua/plugins/outline.lua
@@ -0,0 +1,22 @@
+return {
+  'hedyhli/outline.nvim',
+  config = function()
+    require('outline').setup({
+      outline_window = {
+        position = 'right', -- left / right
+        auto_close = false, -- Closes window if item is selected with enter
+        focus_on_open = false,
+      },
+      outline_items = {
+        show_symbol_details = true, -- Extra symbol details as virtual text
+        show_symbol_lineno = false, -- Line numbers for each symbol
+      },
+      keymaps = {
+        fold_all = { 'W', '<C-h>' },
+        unfold_all = { 'E', '<C-l>' },
+      },
+    })
+
+    vim.keymap.set({ 'n', 'x' }, '<leader>o', '<cmd>Outline<CR>', { desc = 'Toggle outline (symbols)' })
+  end,
+}
