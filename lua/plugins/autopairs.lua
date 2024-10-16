56f37f889141f23c540a7860b038082dffc5670e
gabs <gabs712.g@gmail.com>
2024-10-16 14:22:00 -0300

    Disable pairs on visual block and improve clarity on config

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -8,6 +8,14 @@ return {
         'spectre_panel',
         'harpoon',
       },
+      -- Avoid buggy / iconsistent behavior on those modes
+      disable_in_macro = true,
+      disable_in_replace_mode = true,
+      disable_in_visualblock = true,
+
+      -- Delete pairs with those
+      map_cr = true,
+      map_bs = true,
       map_c_h = true,
       map_c_w = true,
     })
