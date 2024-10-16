b93a55e54722f786232b39acb723b84c83b581b7
gabs <gabs712.g@gmail.com>
2024-10-15 23:38:01 -0300

    Show absolute path for oil on lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,100 +7,115 @@ return {
   config = function()
     local icons = require('custom.icons')
 
-    require('lualine').setup({
-      options = {
-        theme = 'auto',
-        disabled_filetypes = {
-          statusline = {}, -- Only ignores the ft for statusline.
-          winbar = {}, -- Only ignores the ft for winbar.
-          -- Ignores for both \/
-        },
+    local options = {
+      theme = 'auto',
+      disabled_filetypes = {
+        statusline = {}, -- Only ignores the ft for statusline.
+        winbar = {}, -- Only ignores the ft for winbar.
+        -- Ignores for both \/
+      },
 
-        -- Some icons:                    
-        section_separators = { left = '', right = '' },
-        component_separators = { left = '', right = '' },
+      -- Some icons:                    
+      section_separators = { left = '', right = '' },
+      component_separators = { left = '', right = '' },
+    }
+
+    local sections = {
+      lualine_a = {},
+      lualine_b = {
+        'branch',
       },
-      sections = {
-        lualine_a = {},
-        lualine_b = {
-          'branch',
-        },
-        lualine_c = {
-          {
-            'filetype',
-            padding = { left = 1, right = 0 },
-            icon_only = true,
-            icon = {
-              align = 'left',
-            },
-          },
-          {
-            'filename',
-            path = 4, -- Parent directory and file name
-            symbols = {
-              -- Icons:  
-              modified = icons.buffer.modified,
-              readonly = '',
-              unnamed = 'Unnamed',
-              newfile = '',
-            },
-          },
-          -- Show macros
-          {
-            require('noice').api.statusline.mode.get,
-            cond = require('noice').api.statusline.mode.has,
-            color = { fg = '#ff9e64' },
-            padding = { left = 2, right = 1 },
+      lualine_c = {
+        {
+          'filetype',
+          padding = { left = 1, right = 0 },
+          icon_only = true,
+          icon = {
+            align = 'left',
           },
         },
-        lualine_x = {
-          {
-            'searchcount',
-            padding = { left = 1, right = 3 },
-          },
-          {
-            'diagnostics',
-            symbols = {
-              hint = icons.diagnostics.hint,
-              info = icons.diagnostics.info,
-              warn = icons.diagnostics.warn,
-              error = icons.diagnostics.error,
-            },
-          },
-          {
-            'diff',
-            padding = { left = 1, right = 1 },
-            colored = true,
-            symbols = {
-              added = ' ',
-              modified = ' ',
-              removed = ' ',
-            },
-            -- Where to get git info from
-            source = function()
-              -- Using gitsigns, it updates in real time, with no need to save
-              local gitsigns = vim.b.gitsigns_status_dict
-              if gitsigns then
-                return {
-                  added = gitsigns.added,
-                  modified = gitsigns.changed,
-                  removed = gitsigns.removed,
-                }
-              end
-            end,
+        {
+          'filename',
+          path = 4, -- Parent directory and file name
+          symbols = {
+            -- Icons:  
+            modified = icons.buffer.modified,
+            readonly = '',
+            unnamed = 'Unnamed',
+            newfile = '',
           },
         },
-        lualine_y = {
-          {
-            padding = { left = 1, right = 0 },
-            function()
-              return vim.t.maximized and ' ' or ''
-            end,
-          },
-          'progress',
+        -- Show macros
+        {
+          require('noice').api.statusline.mode.get,
+          cond = require('noice').api.statusline.mode.has,
+          color = { fg = '#ff9e64' },
+          padding = { left = 2, right = 1 },
         },
-        lualine_z = {},
       },
+      lualine_x = {
+        {
+          'searchcount',
+          padding = { left = 1, right = 3 },
+        },
+        {
+          'diagnostics',
+          symbols = {
+            hint = icons.diagnostics.hint,
+            info = icons.diagnostics.info,
+            warn = icons.diagnostics.warn,
+            error = icons.diagnostics.error,
+          },
+        },
+        {
+          'diff',
+          padding = { left = 1, right = 1 },
+          colored = true,
+          symbols = {
+            added = ' ',
+            modified = ' ',
+            removed = ' ',
+          },
+          -- Where to get git info from
+          source = function()
+            -- Using gitsigns, it updates in real time, with no need to save
+            local gitsigns = vim.b.gitsigns_status_dict
+            if gitsigns then
+              return {
+                added = gitsigns.added,
+                modified = gitsigns.changed,
+                removed = gitsigns.removed,
+              }
+            end
+          end,
+        },
+      },
+      lualine_y = {
+        {
+          padding = { left = 1, right = 0 },
+          function()
+            return vim.t.maximized and ' ' or ''
+          end,
+        },
+        'progress',
+      },
+      lualine_z = {},
+    }
+
+    -- Custom
+    local oil = {
+      options = vim.deepcopy(options),
+      sections = vim.deepcopy(sections),
+      filetypes = { 'oil' },
+    }
+
+    oil.sections.lualine_c[2].path = 2 -- Absolute path
+    oil.sections.lualine_c[2].symbols.readonly = '' -- Avoids brief flash showing symbol
+
+    require('lualine').setup({
+      options = options,
+      sections = sections,
+      extensions = { oil },
     })
   end,
 }
