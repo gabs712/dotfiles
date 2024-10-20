ea7e4cc779c27098ec06cd35b4097f5b79dbf526
gabs <gabs712.g@gmail.com>
2024-10-19 21:18:35 -0300

    Improve trouble keymaps

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,6 +2,7 @@
 return {
   'folke/trouble.nvim',
   config = function()
+    require('custom.helpers').free_ft_ctrl('trouble')
     require('trouble').setup({
       warn_no_results = false,
       open_no_results = true,
@@ -11,6 +12,7 @@ return {
       keys = {
         ['l'] = 'jump',
         ['h'] = 'fold_close',
+        ['<C-k>'] = 'fold_close',
       },
     })
 
