9b752655e43cd0147d8c320f2fd69ad6406c1ca0
gabs <gabs712.g@gmail.com>
2024-11-02 14:56:01 -0300

    Enable navigation on terminal mode

--- nvim/.config/nvim/lua/plugins/tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/tmux-navigator.lua
@@ -5,9 +5,9 @@ return {
   end,
 
   config = function()
-    vim.keymap.set({ 'n', 'x' }, '<A-h>', '<cmd>TmuxNavigateLeft<cr>')
-    vim.keymap.set({ 'n', 'x' }, '<A-j>', '<cmd>TmuxNavigateDown<cr>')
-    vim.keymap.set({ 'n', 'x' }, '<A-k>', '<cmd>TmuxNavigateUp<cr>')
-    vim.keymap.set({ 'n', 'x' }, '<A-l>', '<cmd>TmuxNavigateRight<cr>')
+    vim.keymap.set({ 'n', 'x', 't' }, '<A-h>', '<cmd>TmuxNavigateLeft<cr>')
+    vim.keymap.set({ 'n', 'x', 't' }, '<A-j>', '<cmd>TmuxNavigateDown<cr>')
+    vim.keymap.set({ 'n', 'x', 't' }, '<A-k>', '<cmd>TmuxNavigateUp<cr>')
+    vim.keymap.set({ 'n', 'x', 't' }, '<A-l>', '<cmd>TmuxNavigateRight<cr>')
   end,
 }
