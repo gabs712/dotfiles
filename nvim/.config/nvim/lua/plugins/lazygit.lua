8f0f7f61308ee96277bcf05f61607fa24c77b542
gabs <gabs712.g@gmail.com>
2024-11-02 10:29:56 -0300

    Esc only quits lazygit inside of neovim

--- nvim/.config/nvim/lua/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -5,5 +5,14 @@ return {
   },
   config = function()
     vim.keymap.set('n', '<leader>l', '<cmd>LazyGit<cr>', { desc = 'LazyGit' })
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'lazygit',
+      callback = function()
+        local current = vim.api.nvim_get_current_buf()
+
+        vim.keymap.set('t', '<Esc>', '<cmd>close<CR>', { buffer = current })
+      end,
+    })
   end,
 }
