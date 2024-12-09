c0e6e29b70db4ffbd88058fafc9da6ccddd26c84
gabs <gabs712.g@gmail.com>
2024-12-09 18:37:28 -0300

    Add jest snippets

--- nvim/.config/nvim/lua/custom/luasnip/snippets.lua
+++ nvim/.config/nvim/lua/custom/luasnip/snippets.lua
@@ -9,3 +9,11 @@ ls.add_snippets('lua', {
   s('lr', fmt("local {} = require('{}')", { rep(1, 'module'), i(1, 'module') })),
   s('vk', fmt("vim.keymap.set({}, '{}', {})", { i(1, 'mode'), i(2, 'keymap'), i(3, 'action') })),
 })
+
+ls.add_snippets('javascript', {
+  s('jd', fmt("describe('{}', () => {{\n  {}\n}})", { i(1), i(2) })),
+  s('jt', fmt("test('{}', () => {{\n  {}\n}})", { i(1), i(2) })),
+  s('ji', fmt("it('{}', () => {{\n  {}\n}})", { i(1), i(2) })),
+  s('j', fmt('expect({}).toBe()', { i(1) })),
+  s('je', fmt('expect({}).toBe()', { i(1) })),
+})
