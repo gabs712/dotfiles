dd4352484796f112f149861b988238971dc1e73b
gabs <gabs712.g@gmail.com>
2025-01-28 15:39:30 -0300

    Notify when toggling hidden on oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -4,6 +4,25 @@ return {
   config = function()
     local oil = require('oil')
 
+    local show_hidden = true
+
+    -- Notify when toggling hidden files
+    local hidden_enabled = show_hidden
+    local notify_toggle_hidden = {
+      callback = function()
+        oil.toggle_hidden()
+        hidden_enabled = not hidden_enabled
+
+        if hidden_enabled then
+          print('Hidden files enabled')
+        else
+          print('Hidden files disabled')
+        end
+      end,
+      mode = 'n',
+      desc = 'Toggle hidden files',
+    }
+
     oil.setup({
       default_file_explorer = true,
       watch_for_changes = true, -- Watch the filesystem for changes and reload oil
@@ -17,7 +36,7 @@ return {
       },
 
       view_options = {
-        show_hidden = true, -- Show files and directories that start with "."
+        show_hidden = show_hidden, -- Show files and directories that start with "."
 
         is_hidden_file = function(name) -- What is considered a hidden file
           return vim.startswith(name, '.')
@@ -37,7 +56,7 @@ return {
 
         ['gd'] = { 'actions.cd', opts = { scope = 'tab' }, desc = 'Change cwd on tab', mode = 'n' },
         ['gr'] = 'actions.refresh',
-        ['g.'] = 'actions.toggle_hidden',
+        ['g.'] = notify_toggle_hidden,
         ['gs'] = 'actions.change_sort',
         ['g?'] = 'actions.show_help',
 
