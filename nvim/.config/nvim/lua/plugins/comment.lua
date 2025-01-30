6c77fae46bccb2eeca94ac75b082025c3333893c
gabs <gabs712.g@gmail.com>
2025-01-30 11:31:55 -0300

    Add nvim-ts-context-commentstring
    Match comments according to cursor position like on jsx files

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,6 +1,15 @@
 return {
-  'numToStr/Comment.nvim',
+  'numToStr/Comment.nvim', -- Enhances comment motions
+  dependencies = {
+    'JoosepAlviste/nvim-ts-context-commentstring', -- Context aware comments (ex. jsx)
+  },
   config = function()
-    require('Comment').setup()
+    require('ts_context_commentstring').setup({
+      enable_autocmd = false,
+    })
+
+    require('Comment').setup({
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+    })
   end,
 }
