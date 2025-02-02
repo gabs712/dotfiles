34b093ceb9169798c90586eae8d050a7fed684a6
gabs <gabs712.g@gmail.com>
2025-02-02 18:30:48 -0300

    Match base and main boilerplates

--- boilerplate/boilerplate/base/babel.config.js
+++ boilerplate/boilerplate/base/babel.config.js
@@ -1,11 +1,15 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 const config = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: 'defaults',
-      },
-    ],
+    isProd
+      ? [
+          '@babel/preset-env',
+          {
+            targets: 'defaults',
+          },
+        ]
+      : {},
   ],
 }
 
