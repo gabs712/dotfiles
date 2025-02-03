2d777cd891af96b2488742e3d01ef025b301b055
gabs <gabs712.g@gmail.com>
2025-02-03 10:06:23 -0300

    Add support for react on eslint

--- boilerplate/boilerplate/main/eslint.config.js
+++ boilerplate/boilerplate/main/eslint.config.js
@@ -1,18 +1,30 @@
 import globals from 'globals'
-import pluginJs from '@eslint/js'
+import js from '@eslint/js'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+
+// REMINDER: eslint-plugin-react-refresh could be useful if using vite or webpack plugin
 
 const config = [
   {
-    ...pluginJs.configs.recommended,
-    files: ['src/**/*.js'],
-  },
-  {
-    files: ['src/**/*.js'],
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+    },
     languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
       globals: globals.browser,
       sourceType: 'module',
     },
     rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+
       'no-unused-vars': 'warn',
       'no-empty': 'warn',
       'no-fallthrough': 'off',
