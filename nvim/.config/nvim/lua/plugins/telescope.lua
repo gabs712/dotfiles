08683715746f2435493538ec503dbfbd0b73018f
gabs <gabs712.g@gmail.com>
2025-02-04 17:04:12 -0300

    Filter <plug> from telescope keymaps function

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -59,8 +59,11 @@ return {
     vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = 'Find old files' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
     vim.keymap.set('n', '<leader>fc', builtin.colorscheme, { desc = 'Find colorschemes' })
-    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Find keymaps' })
     vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = 'Find symbols' })
+
+    vim.keymap.set('n', '<leader>fk', function()
+      builtin.keymaps({ show_plug = false })
+    end, { desc = 'Find keymaps' })
 
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Find help' })
     vim.keymap.set('n', '<leader>fn', function()
