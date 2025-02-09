2560f2500efa093d9479500d27887ce5801b88da
gabs <gabs712.g@gmail.com>
2025-02-08 21:24:01 -0300

    Add jest to eslint

--- boilerplate/boilerplate/vite/eslint.config.js
+++ boilerplate/boilerplate/vite/eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import pluginJest from 'eslint-plugin-jest'
 
 export default [
   { ignores: ['dist'] },
@@ -10,7 +11,10 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...pluginJest.environments.globals.globals,
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -22,6 +26,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      jest: pluginJest,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -33,6 +38,12 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
 
       'no-unused-vars': 'warn',
       'no-empty': 'warn',
