acbb03f2e87438562c796746b1d5c22c818e517f
gabs <gabs712.g@gmail.com>
2025-02-26 10:08:22 -0300

    Add neotest

---
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,68 @@
+return {
+  'nvim-neotest/neotest',
+  dependencies = {
+    'nvim-neotest/nvim-nio',
+    'nvim-lua/plenary.nvim',
+    'antoinemadec/FixCursorHold.nvim',
+    'nvim-treesitter/nvim-treesitter',
+
+    'marilari88/neotest-vitest',
+    'nvim-neotest/neotest-jest',
+  },
+  config = function()
+    require('neotest').setup({
+      adapters = {
+        require('neotest-vitest'),
+        require('neotest-jest')({
+          jestCommand = 'npm test --',
+          jestConfigFile = 'custom.jest.config.ts',
+          env = { CI = true },
+          cwd = function()
+            return vim.fn.getcwd()
+          end,
+        }),
+      },
+      status = {
+        signs = false,
+      },
+      diagnostic = {
+        enabled = false,
+      },
+      output = {
+        open_on_run = false, -- Popup that opens after running tests
+      },
+      summary = {
+        animated = true,
+        count = true, -- Display number of tests found
+        expand_errors = true, -- Expand all errors
+        follow = true, -- Expand on current file
+        mappings = {
+          attach = 'a',
+          clear_marked = 'M',
+          clear_target = 'T',
+          debug = 'd',
+          debug_marked = 'D',
+          expand = { '<C-j>', '<CR>', '<2-LeftMouse>' },
+          expand_all = 'e',
+          help = '?',
+          jumpto = 'i',
+          mark = 'm',
+          next_failed = 'J',
+          output = 'o',
+          prev_failed = 'K',
+          run = 'r',
+          run_marked = 'R',
+          short = 'O',
+          stop = 'u',
+          target = 't',
+          watch = 'w',
+        },
+        open = 'botright vsplit | vertical resize 50',
+      },
+    })
+
+    vim.keymap.set('n', '<leader>t', function()
+      require('neotest').summary.toggle()
+    end)
+  end,
+}
