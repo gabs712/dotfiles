37f0b09fa5db290b7e4de486139e1eab5e6fc02d
gabs <gabs712.g@gmail.com>
2025-02-26 18:20:29 -0300

    Add indentscope to use indent text-objects

--- nvim/.config/nvim/lua/plugins/indent.lua
+++ nvim/.config/nvim/lua/plugins/indent.lua
@@ -1,26 +1,51 @@
 return {
-  'lukas-reineke/indent-blankline.nvim',
-  main = 'ibl',
-  config = function()
-    require('ibl').setup({
-      indent = {
-        char = '▏',
-        tab_char = '▏', -- Still show indent guides if indent is a tab
-      },
-      scope = {
-        enabled = false,
-      },
-      exclude = {
-        filetypes = {
-          'undotree',
-          'harpoon',
-          'neo-tree',
-          'trouble',
-          'text',
-          'markdown',
-          'oil',
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    config = function()
+      require('ibl').setup({
+        indent = {
+          char = '▏',
+          tab_char = '▏', -- Still show indent guides if indent is a tab
         },
-      },
-    })
-  end,
+        scope = {
+          enabled = false,
+        },
+        exclude = {
+          filetypes = {
+            'undotree',
+            'harpoon',
+            'neo-tree',
+            'trouble',
+            'text',
+            'markdown',
+            'oil',
+          },
+        },
+      })
+    end,
+  },
+  {
+    'echasnovski/mini.indentscope',
+    version = '*',
+    config = function()
+      vim.g.miniindentscope_disable = true -- Disable indent highlight
+
+      require('mini.indentscope').setup({
+        options = {
+          try_as_border = true, -- When in indent border selects it's children instead of parent
+          indent_at_cursor = false, -- Selects based on cursor column
+        },
+        mappings = {
+          -- Textobjects
+          object_scope = 'ii',
+          object_scope_with_border = 'ai',
+
+          -- Motions (jump to respective border line)
+          goto_top = '[i',
+          goto_bottom = ']i',
+        },
+      })
+    end,
+  },
 }
