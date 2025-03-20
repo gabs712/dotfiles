cc7633520a4f43b87121f4242be889eacba9c944
gabs <gabs712.g@gmail.com>
2025-03-20 15:35:52 -0300

    Fix wrong tab behavior on newer cmp version

--- nvim/.config/nvim/lua/custom/nvim-cmp/tab.lua
+++ nvim/.config/nvim/lua/custom/nvim-cmp/tab.lua
@@ -13,7 +13,7 @@ M.search = cmp.mapping(function()
 end, { 'c' })
 
 -- Do the same, but automatically accpets single results
-M.cmd = cmp.mapping(function()
+M.cmd = function()
   if not cmp.visible() then
     cmp.complete()
     cmp.select_next_item()
@@ -24,6 +24,20 @@ M.cmd = cmp.mapping(function()
   else
     cmp.select_next_item()
   end
-end, { 'c' })
+end
+
+-- NOTE: Would avoid tab accting as space at the end of completed expressions, but cmp.mapping is acting weird on ':' currently
+-- M.cmd = cmp.mapping(function()
+--   if not cmp.visible() then
+--     cmp.complete()
+--     cmp.select_next_item()
+--
+--     if #cmp.get_entries() == 1 then
+--       cmp.close()
+--     end
+--   else
+--     cmp.select_next_item()
+--   end
+-- end, { 'c' })
 
 return M
