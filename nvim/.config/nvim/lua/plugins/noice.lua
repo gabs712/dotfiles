2765dbabc3de97cecf4df90e3741238646d75596
gabs <gabs712.g@gmail.com>
2025-03-27 17:58:19 -0300

    Modularize noice filters

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -9,62 +9,7 @@ return {
   config = function()
     require('custom.helpers').clear_ctrl_hl('noice')
     require('noice').setup({
-      routes = {
-        -- Disable popups for undos
-        {
-          filter = {
-            event = 'msg_show',
-            find = '(%d+)%s.-;%s.-%s#(%d+)%s%s',
-          },
-          opts = { skip = true },
-        },
-        -- Popups when using "*" or "#" on visual mode
-        {
-          filter = {
-            event = 'msg_show',
-            find = '^%s*W?%s?%[%d+/%d+%]$',
-          },
-          opts = { skip = true },
-        },
-        -- cnext/cprev throws errors when reaching last item, annoying call stack with repeatable movements
-        {
-          filter = {
-            event = 'msg_show',
-            find = 'E553: No more items',
-          },
-          opts = { skip = true },
-        },
-        {
-          filter = {
-            event = 'msg_show',
-            find = 'written$',
-          },
-          opts = { skip = true },
-        },
-        -- DAP
-        {
-          filter = {
-            event = 'notify',
-            find = 'Debug adapter disconnected$',
-          },
-          opts = { skip = true },
-        },
-        {
-          filter = {
-            warning = true,
-            find = 'Adapter reported a frame in .* but: Cursor position outside buffer%. Ensure executable is up2date and if using a source mapping ensure it is correct',
-          },
-          opts = { skip = true },
-        },
-        {
-          filter = {
-            error = true,
-            find = '^No stopped threads. Cannot move$',
-          },
-          opts = { skip = true },
-        },
-      },
-
+      routes = require('custom.noice.notify_filters'),
       lsp = {
         progress = { enabled = false }, -- Disable lsp notifications
         hover = { silent = true }, -- Disable hover notification when not avaliable
