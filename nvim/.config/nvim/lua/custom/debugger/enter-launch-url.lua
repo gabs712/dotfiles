3ec413da95b4214df9ac1089b3638f7dbe59047e
gabs <gabs712.g@gmail.com>
2025-04-06 00:36:38 -0300

    Imrpove enter-launch-url to store most recent entry

--- nvim/.config/nvim/lua/custom/debugger/enter-launch-url.lua
+++ nvim/.config/nvim/lua/custom/debugger/enter-launch-url.lua
@@ -1,12 +1,28 @@
 return function()
+  local input_file = vim.fn.stdpath('data') .. '/last_url.txt'
+  input_file = vim.fs.normalize(input_file)
+
+  local default_url = ''
+  if vim.fn.filereadable(input_file) == 1 then
+    local lines = vim.fn.readfile(input_file)
+    if #lines > 0 then
+      default_url = lines[1]
+    end
+  else
+    vim.fn.writefile({}, input_file)
+  end
+
   local co = coroutine.running()
+
   return coroutine.create(function()
-    vim.ui.input({ prompt = 'Enter URL: ', default = 'http://localhost:' }, function(url)
+    vim.ui.input({ prompt = 'Enter Port', default = default_url }, function(url)
       if url == nil or url == '' then
         return
-      else
-        coroutine.resume(co, url)
       end
+
+      vim.fn.writefile({ url }, input_file)
+
+      coroutine.resume(co, 'http://localhost:' .. url)
     end)
   end)
 end
