8d01d1bc801360645bd91074fcf2cb275c0624ee
gabs <gabs712.g@gmail.com>
2025-04-06 15:25:07 -0300

    Remove todo comment jump bindings

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -17,18 +17,6 @@ return {
       },
     })
 
-    local ts_repeat_move = require('nvim-treesitter.textobjects.repeatable_move')
-    local next_todo, prev_todo = ts_repeat_move.make_repeatable_move_pair(function()
-      require('todo-comments').jump_next()
-    end, function()
-      require('todo-comments').jump_prev()
-    end)
-
-    vim.keymap.set({ 'n', 'x', 'o' }, ']t', next_todo, { desc = 'Next todo ' })
-    vim.keymap.set({ 'n', 'x', 'o' }, '[t', prev_todo, { desc = 'Previous todo' })
-    vim.keymap.set({ 'n', 'x', 'o' }, ']T', next_todo, { desc = 'Next todo ' })
-    vim.keymap.set({ 'n', 'x', 'o' }, '[T', prev_todo, { desc = 'Previous todo' })
-
     vim.keymap.set('n', '<leader>ft', '<cmd>TodoTelescope<CR>', { desc = 'Find todos' })
   end,
 }
