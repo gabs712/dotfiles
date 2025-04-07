89026533c2e2a23cace68eba9c9e724a297ddfe7
gabs <gabs712.g@gmail.com>
2025-04-07 02:13:48 -0300

    Add mini-splitjoin

---
+++ nvim/.config/nvim/lua/plugins/mini-splitjoin.lua
@@ -0,0 +1,11 @@
+return {
+  'echasnovski/mini.splitjoin',
+  version = '*',
+  config = function()
+    require('mini.splitjoin').setup({
+      mappings = {
+        toggle = 'gs',
+      },
+    })
+  end,
+}
