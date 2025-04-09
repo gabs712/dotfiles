f2b77b16d040bfb17870dc04127c9ad7cc19e650
gabs <gabs712.g@gmail.com>
2025-04-09 17:55:19 -0300

    Tweak breakpoint descriptions

--- nvim/.config/nvim/lua/plugins/debugger.lua
+++ nvim/.config/nvim/lua/plugins/debugger.lua
@@ -123,16 +123,16 @@ return {
     end, { desc = 'Clear debugger breakpoints' })
 
     vim.keymap.set('n', '<leader>9', function()
-      dap.set_breakpoint(vim.fn.input('Condition: '))
+      dap.set_breakpoint(vim.fn.input('Condition'))
     end, { desc = 'Set conditional debugger breakpoint' })
 
     vim.keymap.set('n', '<leader>8', function()
-      dap.set_breakpoint(nil, nil, vim.fn.input('Log: '))
+      dap.set_breakpoint(nil, nil, vim.fn.input('Log Point'))
     end, { desc = 'Set log debugger breakpoint' })
 
     vim.keymap.set('n', '<leader>7', function()
-      dap.set_breakpoint(nil, vim.fn.input('Hit condition: '))
-    end, { desc = 'Set hit conditional debugger breakpoint' })
+      dap.set_breakpoint(nil, vim.fn.input('Hit Condition'))
+    end, { desc = 'Set hit condition debugger breakpoint' })
 
     vim.keymap.set('n', '<leader>m', function()
       dapui.toggle({ reset = true })
