264ca98334d30c3a3f2d6e89be0493b4951a7b7c
gabs <gabs712.g@gmail.com>
2025-04-15 01:17:45 -0300

    Tweak cmp sorting comparators

--- nvim/.config/nvim/lua/custom/nvim-cmp/global_comparators.lua
+++ nvim/.config/nvim/lua/custom/nvim-cmp/global_comparators.lua
@@ -1,7 +1,7 @@
 -- Defaults:
 -- cmp.config.compare.offset,
 -- cmp.config.compare.exact,
--- cmp.config.compare.scopes,
+-- -- cmp.config.compare.scopes,
 -- cmp.config.compare.score,
 -- cmp.config.compare.recently_used,
 -- cmp.config.compare.locality,
@@ -14,12 +14,11 @@ local cmp = require('cmp')
 return {
   cmp.config.compare.offset,
   cmp.config.compare.exact,
-  cmp.config.compare.scopes,
   cmp.config.compare.score,
   cmp.config.compare.recently_used,
   cmp.config.compare.locality,
+  cmp.config.compare.order,
   cmp.config.compare.kind,
   cmp.config.compare.sort_text,
   cmp.config.compare.length,
-  cmp.config.compare.order,
 }
