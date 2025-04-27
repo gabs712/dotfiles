b864373d8c8c2c067b8314c31e35aecfd7aa90f5
gabs <gabs712.g@gmail.com>
2025-04-26 23:53:07 -0300

    Ask for dap init file on input

--- nvim/.config/nvim/lua/custom/debugger/find-preferred-file.lua
+++ nvim/.config/nvim/lua/custom/debugger/find-preferred-file.lua
@@ -1,15 +1,40 @@
 return function(preferred_files)
   local root = vim.fn.getcwd()
-  for _, file in ipairs(preferred_files) do
-    local safe_path = vim.fs.normalize(root .. '/' .. file)
+  local default_path = ''
 
-    if vim.fn.filereadable(safe_path) == 1 then
-      vim.notify('Initializing on ' .. file)
-      return file
+  for _, file in ipairs(preferred_files) do
+    local path = vim.fs.normalize(root .. '/' .. file)
+
+    if vim.fn.filereadable(path) == 1 then
+      default_path = file
+      break
     end
   end
 
-  local current_file = vim.fn.expand('%:t')
-  vim.notify('No default - Initializing on ' .. current_file)
-  return '${file}'
+  local co = coroutine.running()
+
+  return coroutine.create(function()
+    vim.ui.input({ prompt = 'Enter Port', default = default_path }, function(input)
+      if input == nil then
+        return
+      end
+
+      if input == '' then
+        local current_file = vim.fn.expand('%:t')
+        vim.notify('Initializing on ' .. current_file)
+
+        coroutine.resume(co, '${file}')
+        return
+      end
+
+      if vim.fn.filereadable(input) == 1 then
+        vim.notify('Initializing on ' .. input)
+
+        coroutine.resume(co, input)
+        return
+      end
+
+      vim.notify('Cannot find module ' .. vim.fs.normalize(root .. '/' .. input), vim.log.levels.WARN)
+    end)
+  end)
 end
