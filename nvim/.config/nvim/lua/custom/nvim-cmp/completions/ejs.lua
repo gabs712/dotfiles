92d8a995c188f9d7a2c79ff1b438ffc0f0337a24
gabs <gabs712.g@gmail.com>
2025-04-30 00:33:49 -0300

    Only shows ejs buffer completions inside of ejs tags

--- nvim/.config/nvim/lua/custom/nvim-cmp/completions/ejs.lua
+++ nvim/.config/nvim/lua/custom/nvim-cmp/completions/ejs.lua
@@ -1,11 +1,23 @@
 local cmp = require('cmp')
+local ts_utils = require('nvim-treesitter.ts_utils')
 
 local emmet_on_element = require('custom.nvim-cmp.completions.utils.emmet-on-elemnt')
 
 return {
   sources = cmp.config.sources({
     { name = 'luasnip' },
-    { name = 'buffer' },
+    {
+      name = 'buffer',
+      entry_filter = function()
+        local cursor_node = ts_utils.get_node_at_cursor()
+
+        if cursor_node:type() == 'ERROR' then
+          return true
+        end
+
+        return false
+      end,
+    },
     {
       name = 'nvim_lsp',
       entry_filter = emmet_on_element,
