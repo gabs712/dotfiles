7bd59f59cb8662be7658d85e0b8bf8dea3fd682e
gabs <gabs712.g@gmail.com>
2025-05-02 16:24:08 -0300

    Improve harpoon notification

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -13,9 +13,28 @@ return {
     vim.keymap.set('n', '<C-q>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = 'Harpoon' })
+
     vim.keymap.set('n', '<C-m>', function() -- Also is triggered by CR
+      for _, item in pairs(harpoon:list().items) do
+        local item_path = vim.fs.normalize(item.value)
+
+        if not vim.startswith(item_path, '/') then
+          local root = vim.fn.getcwd()
+          item_path = vim.fs.normalize(root .. '/' .. item_path)
+        end
+
+        local buf_path = vim.fs.normalize(vim.api.nvim_buf_get_name(0))
+
+        if buf_path == item_path then
+          vim.notify('Mark already settled')
+          return
+        end
+      end
+
       harpoon:list():add()
-      vim.notify('Harpoon mark settled')
+      local last_index = #harpoon:list().items
+
+      vim.notify('Mark added at index ' .. last_index)
     end, { desc = 'Set harpoon mark' })
 
     -- Harpoon sometimes jumps to a previously indexed position when using a mark
