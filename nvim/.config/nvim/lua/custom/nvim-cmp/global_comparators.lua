00b7a85161a70e37f23e14f104d1423ec2596b86
gabs <gabs712.g@gmail.com>
2025-05-02 20:49:03 -0300

    Tweak comparators

--- nvim/.config/nvim/lua/custom/nvim-cmp/global_comparators.lua
+++ nvim/.config/nvim/lua/custom/nvim-cmp/global_comparators.lua
@@ -1,4 +1,4 @@
--- Defaults:
+-- Defaults?
 -- cmp.config.compare.offset,
 -- cmp.config.compare.exact,
 -- -- cmp.config.compare.scopes,
@@ -16,9 +16,9 @@ return {
   cmp.config.compare.exact,
   cmp.config.compare.score,
   cmp.config.compare.recently_used,
+  cmp.config.compare.length,
   cmp.config.compare.locality,
   cmp.config.compare.order,
   cmp.config.compare.kind,
   cmp.config.compare.sort_text,
-  cmp.config.compare.length,
 }
