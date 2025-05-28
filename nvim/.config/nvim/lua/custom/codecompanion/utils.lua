fc2b2f0efeff530f937eb0d99351490416f77b4d
gabs <gabs712.g@gmail.com>
2025-05-28 20:19:26 -0300

    Fix nav_heading

--- nvim/.config/nvim/lua/custom/codecompanion/utils.lua
+++ nvim/.config/nvim/lua/custom/codecompanion/utils.lua
@@ -3,9 +3,9 @@ local M = {}
 local ts = require('codecompanion.utils.treesitter')
 
 M.nav_heading = function(wich)
-  vim.cmd("normal! m'")
-
   local count = vim.v.count > 0 and vim.v.count or 1
+
+  vim.cmd("normal! m'")
   ts.goto_heading(wich, count)
 end
 
