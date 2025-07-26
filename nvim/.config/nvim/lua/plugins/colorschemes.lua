18d6d82c4c7c31183bc26c87ef87bb0c382e46a0
gabs <gabs712.g@gmail.com>
2025-07-26 15:08:41 -0300

    Cleanup unsued colorschemes

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,6 +1,6 @@
 return {
   {
-    'folke/tokyonight.nvim',
+    'EdenEast/nightfox.nvim',
   },
   {
     'catppuccin/nvim',
@@ -14,55 +14,9 @@ return {
     end,
   },
   {
-    'rebelot/kanagawa.nvim',
-  },
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-  },
-  {
-    'EdenEast/nightfox.nvim',
-  },
-  {
-    'bluz71/vim-moonfly-colors',
-    name = 'moonfly',
-  },
-  {
-    'bluz71/vim-nightfly-colors',
-    name = 'nightfly',
-  },
-  {
-    'arcticicestudio/nord-vim',
-  },
-  {
-    'HoNamDuong/hybrid.nvim',
-  },
-  {
-    'navarasu/onedark.nvim',
+    'folke/tokyonight.nvim',
   },
   {
     'projekt0n/github-nvim-theme',
-  },
-  {
-    'AlexvZyl/nordic.nvim',
-  },
-  {
-    'cdmill/neomodern.nvim', -- 'iceclimber' | 'gyokuro' | 'hojicha' | 'roseprime'
-    config = function()
-      require('neomodern').setup()
-    end,
-  },
-  {
-    'cocopon/iceberg.vim',
-    config = function()
-      vim.api.nvim_create_autocmd('ColorScheme', {
-        pattern = 'iceberg',
-        callback = function()
-          -- Normally StatusLine is too bright
-          vim.api.nvim_set_hl(0, 'StatusLine', { bg = '#818596', fg = '#1e2132' })
-          vim.api.nvim_set_hl(0, 'StatusLineTerm', { bg = '#818596', fg = '#1e2132' })
-        end,
-      })
-    end,
   },
 }
--- nvim/.config/nvim/lua/plugins/themery.lua
+++ nvim/.config/nvim/lua/plugins/themery.lua
@@ -9,43 +9,19 @@ return {
     require('custom.helpers').map_ctrl_j_hl('themery')
     require('themery').setup({
       themes = {
+        'duskfox',
+        'nightfox',
+
         'catppuccin-mocha',
         'catppuccin-macchiato',
         'catppuccin-frappe',
-
-        'duskfox',
-        'nordfox',
-        'nightfox',
-        'carbonfox',
-
-        'iceclimber',
-        'roseprime',
-
-        'hybrid',
-
-        'iceberg',
-
-        'nord',
-        'nordic',
-
-        'moonfly',
-
-        'nightfly',
-
-        'github_dark_default',
-        'github_dark_dimmed',
-
-        'rose-pine-main',
-        'rose-pine-moon',
-
-        'onedark',
 
         'tokyonight-moon',
         'tokyonight-night',
         'tokyonight-storm',
 
-        'kanagawa-wave',
-        'kanagawa-dragon',
+        'github_dark_default',
+        'github_dark_dimmed',
       },
       livePreview = true,
     })
