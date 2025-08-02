140b67c688a4f07d6910963cf550a5d132935c45
gabs <gabs712.g@gmail.com>
2025-08-02 20:29:48 -0300

    Improve hl and popup clear when pressing esc
    It now has intuitive behavior, priorititizing to clear popups if any,
    else it clears highlights. Inside of popups, it has opposite effect.

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -20,17 +20,39 @@ vim.keymap.del('x', 'gra')
 vim.keymap.del('n', 'grn')
 vim.keymap.del('n', 'gri')
 
--- Dismiss highlights and floating windows
 vim.keymap.set('n', '<Esc>', function()
-  vim.cmd('nohlsearch')
+  local hl_active = vim.v.hlsearch ~= 0
+  local ext_win_closed = false
 
   for _, win in pairs(vim.api.nvim_list_wins()) do
-    if vim.api.nvim_win_is_valid(win) and vim.api.nvim_win_get_config(win).relative ~= '' then
-      local buf = vim.api.nvim_win_get_buf(win)
-      if vim.api.nvim_buf_get_option(buf, 'filetype') ~= 'AvanteConfirm' then
-        vim.api.nvim_win_close(win, false)
+    local is_float_win = vim.api.nvim_win_is_valid(win) and vim.api.nvim_win_get_config(win).relative ~= ''
+
+    if is_float_win then
+      local is_notification = vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(win), 'filetype') == 'notify'
+
+      if is_notification and hl_active then
+        vim.cmd('nohlsearch')
+        goto continue
       end
+
+      local on_win = win == vim.api.nvim_get_current_win()
+
+      if on_win and hl_active then
+        return vim.cmd('nohlsearch')
+      end
+
+      if on_win then
+        return vim.api.nvim_win_close(win, false)
+      end
+
+      vim.api.nvim_win_close(win, false)
+      ext_win_closed = true
     end
+    ::continue::
+  end
+
+  if hl_active and not ext_win_closed then
+    vim.cmd('nohlsearch')
   end
 end, { desc = 'Clear highlights and popups' })
 
