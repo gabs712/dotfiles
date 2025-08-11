0e378cf088b5624aa33f47c6fc920325298b10d1
gabs <gabs712.g@gmail.com>
2025-08-11 20:01:24 -0300

    Tweak lazygit opts

--- nvim/.config/nvim/lua/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -4,13 +4,6 @@ return {
     'nvim-lua/plenary.nvim',
   },
   config = function()
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = 'lazygit',
-      callback = function()
-        vim.keymap.set('t', '<Esc>', '<C-c>', { buffer = true, remap = true })
-      end,
-    })
-
     vim.keymap.set('n', '<leader>l', '<cmd>LazyGit<cr>', { desc = 'LazyGit' })
   end,
 }
