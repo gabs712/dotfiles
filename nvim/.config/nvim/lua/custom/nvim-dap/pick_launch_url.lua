589487a49e439fa2915f7cb8d5bfd4e242c6bee8
gabs <gabs712.g@gmail.com>
2025-08-21 19:55:54 -0300

    refactor(dap): update pick_launch_url to use new storage api

--- nvim/.config/nvim/lua/custom/nvim-dap/pick_launch_url.lua
+++ nvim/.config/nvim/lua/custom/nvim-dap/pick_launch_url.lua
@@ -1,27 +1,15 @@
 return function()
-  local input_file = vim.fn.stdpath('data') .. '/last_url.txt'
-  input_file = vim.fs.normalize(input_file)
-
-  local default_url = ''
-  if vim.fn.filereadable(input_file) == 1 then
-    local lines = vim.fn.readfile(input_file)
-    if #lines > 0 then
-      default_url = lines[1]
-    end
-  else
-    vim.fn.writefile({}, input_file)
-  end
+  local storage = require('utils.storage')
+  local default_url = storage.get_field('last_url') or ''
 
   local co = coroutine.running()
-
   return coroutine.create(function()
     vim.ui.input({ prompt = 'Enter Port', default = default_url }, function(port)
       if port == nil or port == '' then
         return
       end
 
-      vim.fn.writefile({ port }, input_file)
-
+      storage.set_field('last_url', port)
       coroutine.resume(co, 'http://localhost:' .. port)
     end)
   end)
