3045382c7adb2a0a3e7f17f5c522941d117bafe2
gabs <gabs712.g@gmail.com>
2025-08-22 01:46:46 -0300

    refactor(dap): change browser url storage field

--- nvim/.config/nvim/lua/custom/nvim-dap/pick_browser_url.lua
+++ nvim/.config/nvim/lua/custom/nvim-dap/pick_browser_url.lua
@@ -1,15 +1,15 @@
 return function()
   local storage = require('utils.storage')
-  local default_url = storage.get_field('last_url') or ''
+  local default_url = storage.get_field('dap_browser_port') or ''
 
   local co = coroutine.running()
   return coroutine.create(function()
-    vim.ui.input({ prompt = 'Launch on port: ', default = default_url }, function(port)
+    vim.ui.input({ prompt = 'Launch on port', default = default_url }, function(port)
       if port == nil or port == '' then
         return
       end
 
-      storage.set_field('last_url', port)
+      storage.set_field('dap_browser_port', port)
       coroutine.resume(co, 'http://localhost:' .. port)
     end)
   end)
